2f1c69a5335b11e51bd31f54e7425610eddb2974
AdrianJPT <86939628+AdrianJPT@users.noreply.github.com>
2023-03-10 23:23:48 -0500

    Delete FLASK directory

--- FLASK/credentials.py
+++
@@ -1,8 +0,0 @@
-Netbox_Url = '192.168.1.203'
-Netbox_Token = '4c121d2ed2103e10fd2f4d9532dcb8e040fd0fc9'
-
-Zabbix_Url = 'http://18.229.126.124/zabbix/api_jsonrpc.php'
-Zabbix_Token = '64acb78476350f099de1ea79800e2901ffb5a267f7aef5ac3e7c4d1af167e42a'
-
-Zabbix_User = 'Admin'
-Zabbix_Password = 'zabbix'
