c6657728811701fe97ec0537f06cdab910422f90
AdrianJPT <86939628+AdrianJPT@users.noreply.github.com>
2023-03-10 23:56:14 -0500

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-Netbox_Url = '192.168.1.203'
+Netbox_Url = 'http://192.168.1.203/'
 Netbox_Token = '4c121d2ed2103e10fd2f4d9532dcb8e040fd0fc9'
 
 Zabbix_Url = 'http://18.229.126.124/zabbix/api_jsonrpc.php'
