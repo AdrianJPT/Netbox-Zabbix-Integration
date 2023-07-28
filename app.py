561d6a383260e5b33f8acae9a5b6bae35f0314da
AdrianJPT <86939628+AdrianJPT@users.noreply.github.com>
2023-07-28 15:38:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,10 +20,7 @@ app = Flask(__name__)
 ZABBIX_SERVER = Zabbix_Url
 
 zapi = ZabbixAPI(ZABBIX_SERVER)
-zapi.login(Zabbix_User, Zabbix_Password)
-
-zabbix_url = str(Zabbix_Url)
-zabbix_token = Zabbix_Token
+zapi.login(api_token=Zabbix_Token)
 
 
 @app.route('/create', methods=['POST'])
@@ -31,7 +28,10 @@ def zabbix_host_create():
 
     ## LEER REQUEST DE NETBOX
     if(request.data):
+
         reading_post = request.get_json()
+        print(reading_post)
+        
         nb_device_name = reading_post['data']['name']
         nb_site_name = reading_post['data']['site']['name']
     
@@ -54,7 +54,7 @@ def zabbix_host_create():
                     'main': 1,
                     'useip': 0,
                     'ip': '',
-                    'dns': 'UPDATE_IP',
+                    'dns': 'netbox-zabbix-PrimaryIP',
                     'port': '9999'
                 }],
                 groups=[{'groupid': find_hostgroup[0]['groupid']}],
@@ -83,7 +83,7 @@ def zabbix_host_create():
                             'main': 1,
                             'useip': 0,
                             'ip': '',
-                            'dns': 'UPDATE_IP',
+                            'dns': 'netbox-zabbix-PrimaryIP',
                             'port': '9999'
                         }],
                         groups = [{"groupid": find_hostgroup[0]['groupid']}])
@@ -100,25 +100,27 @@ def zabbix_host_create():
 @app.route('/update', methods=['POST'])
 def zabbix_host_update():
     if(request.data):
-
+        
         # LEER NOMBRE DEL DEVICE NETBOX
         reading_post = request.get_json()
-        
+        #print(reading_post)
         # Mandatory
         nb_device_name = reading_post['data']['name']
-        nb_device_name = reading_post['data']['name']
-        nb_device_role = ""
+        pre_site = reading_post['snapshots']['prechange']['site']
+        post_site = reading_post['snapshots']['postchange']['site']
+        
         # OPTIONAL
+        pre_platform = reading_post['snapshots']['prechange']['platform']
+        post_platform = reading_post['snapshots']['postchange']['platform']
         
-        
-        # TRAER LA INFO DE ZABBIX
-        hosts_zabbix = zapi.host.get(output=["hostid", "name", "interfaces"], filter={"name": nb_device_name}, selectInterfaces=["interfaceid", "ip", "dns"], selectGroups=["groupid"])
-        
+        pre_primary_ipv4 = reading_post['snapshots']['prechange']['primary_ip4']
+        post_primary_ipv4 = reading_post['snapshots']['postchange']['primary_ip4']
+                
         # Update Platform 
-        za.update_Template_Host(nb_device_name, reading_post['snapshots']['prechange']['platform'], reading_post['snapshots']['postchange']['platform'])
+        za.update_Template_Host(nb_device_name, pre_platform, post_platform)
         
         # Update Site
-        
+        za.update_HostGroup_Host(nb_device_name, pre_site, post_site)
         
         # Change name
         if(reading_post['snapshots']['prechange']['name'] != nb_device_name):
@@ -126,124 +128,8 @@ def zabbix_host_update():
             
             za.update_Hostname(before_nb_device_name, nb_device_name)
         
-        # Add and IP
-        
-        # Remove and IP 
-    
-        # ADD IP  
-        
-        
-        # SI YA EXISTIA EL NB DEVICE Y EN ZABBIX NO, SE CREARA UNO
-        if len(hosts_zabbix) != 0:
-            # SI EXISTE EN ZABBIX
-            zab_host_id = hosts_zabbix[0]['hostid']     
-            # IDENTIFICAR SI EXISTE ALGUNA INTERFAZ
-            zab_host_int = hosts_zabbix[0]['interfaces']
-
-            if len(zab_host_int) != 0:
-                print(f'Interface device exists')
-                # OBTNER EL ID DE LA INTERFAZ
-                zab_host_int_id = hosts_zabbix[0]['interfaces'][0]['interfaceid']
-
-                # VALIDAR SI SE ESTA QUITANDO O ACTUALIZANDO LA IP
-                if reading_post['data']['primary_ip'] != None:
-                    print("Updating interface")
-
-                    request_nb_primary_ip = reading_post['data']['primary_ip']['address']
-                    convert_to_ip = ipaddress.IPv4Interface(request_nb_primary_ip)
-                    nb_primary_ip = str(convert_to_ip.ip)
-
-                    zapi.hostinterface.update(interfaceid=zab_host_int_id, ip=nb_primary_ip)
-
-                    print(f"ZABBIX | SUCCESS | IP updated: ({nb_primary_ip}), Interface id: ({zab_host_int_id})")
-
-                else:
-                    # BORRANDO INTERFAZ ZAbbix HOST
-                    
-                    requests.put(zabbix_url, json={
-                            "jsonrpc": "2.0",
-                            "method": "hostinterface.delete",
-                            "params": [
-                                zab_host_int_id
-                            ],
-                            "auth": zabbix_token,
-                            "id": 1})
-                    print(f"ZABBIX | SUCCESS | Remove IP from host({nb_device_name})")
-            else:           
-            # NO TIENE INTERFAZ CREAR UNA Y ASOCIAR UNA IP
-                try:
-                    # QUITAR LA MASK del CIDR, VALIDAR LA IP EN EL REQUEST Y ACTUALIZAR
-                    request_nb_primary_ip = reading_post['data']['primary_ip']['address']
-                    convert_to_ip = ipaddress.IPv4Interface(request_nb_primary_ip)
-                    nb_primary_ip = str(convert_to_ip.ip)
-                    
-                    # AGREGAR LA INTERFAZ Y IP AL ZABBIX HOST
-                    print("ooooooooooooooooooooooooooo_1") #AQUI NO ES
-                    zapi.host.update(hostid = zab_host_id, name = nb_device_name, interfaces = [{
-                                                    
-                                                    "type": 1,
-                                                    "main": 1,
-                                                    "useip": 1, 
-                                                    "ip": nb_primary_ip,
-                                                    "dns": "",
-                                                    "port": "10050"
-                                                }]
-                                                ,
-                                                groups = [
-                                                    {
-                                                        "groupid": "1"
-                                                    }
-                                                ]
-                                                )
-                    print(f"ZABBIX | SUCCESS | IP updated ({nb_primary_ip}) in Zabbix Server")
-
-                except Exception as e:
-                    print("ZABBIX | WARNING | ", e.__class__, f"ocurrio para ({nb_device_name}).")
-                    print(f"ZABBIX | SUCCESS | IP: ({nb_primary_ip}) ")
-                    print(f"ZABBIX | WARNING | IP not found in netbox")
-        
-        #CUANDO NO EXITE EN ZABBIX PERO EL NETBOX SÍ
-        else:
-
-            # VER SI CREAR CON IP O SIN IP
-            if reading_post['data']['primary_ip'] != None: 
-                # CREAR CON IP
-                print("Creating IP")
-                request_nb_primary_ip = reading_post['data']['primary_ip']['address']
-                convert_to_ip = ipaddress.IPv4Interface(request_nb_primary_ip)
-                nb_primary_ip = str(convert_to_ip.ip)            
-
-                print("ooooooooooooooooooooooooooo_2") #AQUI NO ES
-                zapi.host.create(host = nb_device_name,
-                    groups = [
-                                {
-                                    "groupid": "1"
-                                }
-                            ],
-                    interfaces = [{
-                                                
-                        "type": 1,
-                        "main": 1,
-                        "useip": 1,
-                        "ip": nb_primary_ip,
-                        "dns": "",
-                        "port": "10050"
-                    }])
-
-                print(f"ZABBIX | SUCCESS | Host ({nb_device_name}) created with ip ({nb_primary_ip})")
-
-            else:
-                # CREAR SOLAMENTE EL HOST SIN IP
-                print("CREAND SIN IP")
-                zapi.host.create(host = nb_device_name,
-                    groups = [
-                                {
-                                    "groupid": "1"
-                                }
-                            ]
-                            )
-                
-                print(f"ZABBIX | SUCCESS | Se creo el host ({nb_device_name}) , porque no exitia en Zabbix Server")
+        # Manage IP Address
+        za.update_ips_host(nb_device_name, pre_primary_ipv4, post_primary_ipv4)
 
 
         
@@ -260,7 +146,7 @@ def zabbix_host_delete():
 
         hosts_zabbix = zapi.host.get(output=["hostid", "name", "interfaces"], filter={"name": nb_device_name}, selectInterfaces=["interfaceid", "ip", "dns"], selectGroups=["groupid"])
 
-        zab_host_id = hosts_zabbix[0]['hostid']     # IDENTIFICAR AL HOST POR EL ID Y EL NETBOX NAME
+        zab_host_id = hosts_zabbix[0]['hostid']     # IDENTIFY The host ID 
         
         print("")
         print(f"ZABBIX | Eliminando ... ({nb_device_name})")
