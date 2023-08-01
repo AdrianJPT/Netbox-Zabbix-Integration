2ced39376c353b0d0836150dff055c72e1d6921d
AdrianJPT <86939628+AdrianJPT@users.noreply.github.com>
2023-08-01 02:16:45 -0500

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -4,5 +4,3 @@ Netbox_Token = '24d3fcabf87219519131bd37f5501663201859ed'
 Zabbix_Url = 'http://4.228.209.251:80/'
 Zabbix_Token = '3b268ca177be445f0d1ea8dc0ffde327248966adi'
 
-Zabbix_User = 'Admin'
-Zabbix_Password = 'zabbix'
